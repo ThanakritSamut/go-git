c7a727905df44d9dd367ea02dc0f335aac709a45
Thanakrit Samut <thanakrit_samut@hotmail.com>
2023-11-26 20:12:04 +0700

    -feature :sparkles: add dev

--- main.go
+++ main.go
@@ -14,10 +14,15 @@ func mul(a ,b int )int {
 	return a*b
 }
 
+func dev(a, b float64 )float64 {
+	return a / b
+}
+
 func main() {
 	fmt.Println("Hello Github")
 	fmt.Println(sum(1,2))
 	fmt.Println(sub(1,2))
 	fmt.Println(mul(1,2))
+	fmt.Println(dev(1,2))
 
 }
